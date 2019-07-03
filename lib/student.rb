29b45f113c802e58d28e01ad9c22e44549e7ef6a
Keith Isham <kisham8899@gmail.com>
2019-07-03 16:43:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,13 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr_accessor :name, :grade
   
+  def initialize(name, id, grade)
+    @name = name
+    @grade = 
+    @id = id
+  end
   
   
   
